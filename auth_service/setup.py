482c730ab2323d6814d5def36679e1f6a075fd02
unfettered-one <103358367+unfettered-one@users.noreply.github.com>
2025-08-21 00:00:54 +0530

    feat: add my own custom errorhub lib (#1)

--- auth_service/setup.py
+++ auth_service/setup.py
@@ -4,13 +4,7 @@ setup(
     name="auth_service",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        "fastapi==0.116.1",
-        "uvicorn==0.35.0",
-        "pydantic",
-        "dotenv"
-
-    ],
+    install_requires=["uvicorn==0.35.0", "errorhub==0.1.0"],
     entry_points={
         "console_scripts": [],
     },
