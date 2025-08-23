f94594f4bc914c129f21b11e0a7a18cd8ca596b0
unfettered-one <pakhaleyash7@gmail.com>
2025-08-23 13:29:32 +0000

    feat: use lib and fix main.py

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from errorhub.exceptions import ErrorHubException
 
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="Auth-Service",
     description="API for Auth-Service",
-    version="0.0.1",
+    version="0.1.0",
 )
 app.add_middleware(
     CORSMiddleware,
@@ -14,3 +16,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# Global Exception Handler
+@app.exception_handler(ErrorHubException)
+async def errorhub_exception_handler(exc: ErrorHubException):
+    """
+    Catch all ErrorHubExceptions and return structured JSON response.
+    """
+    return JSONResponse(status_code=exc.error_detail.code, content=exc.to_dict())
--- auth_service/setup.py
+++ auth_service/setup.py
@@ -4,7 +4,7 @@ setup(
     name="auth_service",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["uvicorn==0.35.0", "errorhub==0.1.0"],
+    install_requires=["uvicorn==0.35.0", "errorhub==0.1.8"],
     entry_points={
         "console_scripts": [],
     },
