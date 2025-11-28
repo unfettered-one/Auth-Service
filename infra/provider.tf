8bdf19b7c04711008fe038581d513ce8deeaef0d
unfettered-one <pakhaleyash7@gmail.com>
2025-11-28 21:17:09 +0000

    add terrafrom providers

--- infra/provider.tf
+++ infra/provider.tf
@@ -15,3 +15,13 @@ provider "aws" {
     }
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+      configuration_aliases = [ aws ]
+    }
+  }
+}
